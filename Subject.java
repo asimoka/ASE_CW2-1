8b8611557aa104fdf01397aa0b31bbed15128012
Kirsty <kirstyroxanaduncan@gmail.com>
2017-03-19 21:24:44 +0000

    Delete Subject.java

--- Subject.java
+++
@@ -1,8 +0,0 @@
-package ASE_CW2;
-
-public interface Subject 
-{
-    public void registerObserver( Observer obs);
-    public void removeObserver( Observer obs);
-    public void notifyObservers();
-}
