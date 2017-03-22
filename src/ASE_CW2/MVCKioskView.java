f8235724ef1235e0068f4aa3fdcff87a2d7f11ed
KirstyRD <krd1@hw.ac.uk>
2017-03-22 13:43:12 +0000

    extra windows

--- src/ASE_CW2/MVCKioskView.java
+++ src/ASE_CW2/MVCKioskView.java
@@ -11,18 +11,21 @@ import javax.swing.border.EmptyBorder;
 public class MVCKioskView extends JFrame implements Observer, ActionListener{
 	
 	//instance variables
-	private Thread workerThread1;
-	private Thread workerThread2;
-	private Worker worker1, worker2;
+	private Thread workerThread1, workerThread2, workerThread3, workerThread4;
+	private Worker worker1, worker2, worker3, worker4;
 	private ListsManager listMan;
 	private Kiosk kiosk;
 	
-	private JLabel passLabel, taxiLabel; 
-	private String output1, output2;
-	private JTextArea area1, area2, area3, area4;
-	Color bg = new Color(188,238,255);
+	private String output1, output2, output3, output4;
+	private JTextArea area1, area2, area3, area4, areaPassengers, areaTaxis;
+	private JRadioButton fast, medium, slow;
+	//colours
+	Color bgC = new Color(218,251,255);
+	Color stopC = new Color(255,87,98);
+	Color goC = new Color(0,203,235);
+	Color btnC = new Color(164,244,255);
 	
-	private JButton go, pause1, pause2, stop, resetPass, resetTaxi;
+	private JButton go, pause1, pause2, pause3, pause4, stop, resetPass, resetTaxi;
 	
 	public MVCKioskView() {
 		//reads passenger groups and taxi details from the GUI
@@ -32,11 +35,15 @@ public class MVCKioskView extends JFrame implements Observer, ActionListener{
     	//consumer objects, two worker threads
     	worker1 = new Worker(kiosk);
     	worker2 = new Worker(kiosk);
+    	worker3 = new Worker(kiosk);
+    	worker4 = new Worker(kiosk);
     	//register workers as subjects to be observed by GUI
 		worker1.registerObserver(this);
 		worker2.registerObserver(this);
+		worker3.registerObserver(this);
+		worker4.registerObserver(this);
 		//GUI details
-		setSize(500,500);
+		setSize(520,650);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setTitle("Tax Kiosk");
@@ -53,26 +60,26 @@ public class MVCKioskView extends JFrame implements Observer, ActionListener{
 	//North panel contains buttons to start the simulation, stop it and reset
 	public void setupPanelNorth() {
 		JPanel nPanel = new JPanel(new FlowLayout());
+		nPanel.setBackground(bgC);
 		
 		go = new JButton("BEGIN SIMULATION");
-		//Color goColor = new Color(0,188,255);
-		//go.setBackground(goColor);
+		go.setBackground(goC);
         go.addActionListener(this);
         nPanel.add(go);
         
         stop = new JButton("STOP");
-        //stop.setBackground(Color.red);
+        stop.setBackground(stopC);
         stop.addActionListener(this);
         nPanel.add(stop);
         
-        resetPass = new JButton("MORE PASSENGERS");
+        resetPass = new JButton("ADD PASSENGERS");
         resetPass.addActionListener(this);
-        //resetPass.setBackground(bg);
+        resetPass.setBackground(btnC);
         nPanel.add(resetPass);
 
-        resetTaxi = new JButton("MORE TAXIS");
+        resetTaxi = new JButton("ADD TAXIS");
         resetTaxi.addActionListener(this);
-        //resetTaxi.setBackground(bg);
+        resetTaxi.setBackground(btnC);
         nPanel.add(resetTaxi);
         
         this.add(nPanel, BorderLayout.NORTH);
@@ -81,13 +88,14 @@ public class MVCKioskView extends JFrame implements Observer, ActionListener{
 	public void setupPanelWest(){
 		JPanel nPanel = new JPanel(new GridLayout(0,1));
 		nPanel.setBorder(new EmptyBorder(15,15,15,15));
-		nPanel.setBackground(bg);
+		nPanel.setBackground(bgC);
 		nPanel.setSize(250,50);
 		
 		JLabel label1 = new JLabel("Window 1");
 		nPanel.add(label1);
 		
         pause1 = new JButton("PAUSE");
+        pause1.setBackground(btnC);
         pause1.addActionListener(this);
         nPanel.add(pause1);
 		
@@ -98,6 +106,22 @@ public class MVCKioskView extends JFrame implements Observer, ActionListener{
         area1.setWrapStyleWord(true);
         
 		nPanel.add(area1);
+		
+		
+		JLabel label3 = new JLabel("Window 3");
+		nPanel.add(label3);
+		
+        pause3 = new JButton("PAUSE");
+        pause3.setBackground(btnC);
+        pause3.addActionListener(this);
+        nPanel.add(pause3);
+		
+		area3 = new JTextArea(" ");
+		area3.setSize(200,80);
+		area3.setEditable(false);
+		area3.setLineWrap(true);
+        area3.setWrapStyleWord(true);
+		nPanel.add(area3);
 
 		this.add(nPanel, BorderLayout.WEST);
 	}
@@ -106,13 +130,14 @@ public class MVCKioskView extends JFrame implements Observer, ActionListener{
 
 		JPanel nPanel = new JPanel(new GridLayout(0,1));
 		nPanel.setBorder(new EmptyBorder(15,15,15,15));
-		nPanel.setBackground(bg);
+		nPanel.setBackground(bgC);
 		nPanel.setSize(250,50);
 		
 		JLabel label2 = new JLabel("Window 2");
 		nPanel.add(label2);
 
         pause2 = new JButton("PAUSE");
+        pause2.setBackground(btnC);
         pause2.addActionListener(this);
         nPanel.add(pause2);
 		
@@ -121,39 +146,58 @@ public class MVCKioskView extends JFrame implements Observer, ActionListener{
 		area2.setEditable(false);
 		area2.setLineWrap(true);
         area2.setWrapStyleWord(true);
-
+    
 		nPanel.add(area2);
+		
+		
+		JLabel label4 = new JLabel("Window 4");
+		nPanel.add(label4);
+		
+        pause4 = new JButton("PAUSE");
+        pause4.setBackground(btnC);
+        pause4.addActionListener(this);
+        nPanel.add(pause4);
+		
+		area4 = new JTextArea(" ");
+		area4.setSize(200,80);
+		area4.setEditable(false);
+		area4.setLineWrap(true);
+        area4.setWrapStyleWord(true);
+		nPanel.add(area4);
+		
 		this.add(nPanel, BorderLayout.EAST);
 	}
 	
 	public void setupPanelCenter() {
 		JPanel nPanel = new JPanel();
-		nPanel.setBackground(bg);
+		nPanel.setBackground(bgC);
+		
 		this.add(nPanel, BorderLayout.CENTER);
 	}
 	
 	public void setupPanelSouth() {
 
 		JPanel nPanel = new JPanel();
-		nPanel.setBackground(bg);
+		nPanel.setBackground(bgC);
 		
-		area3 = new JTextArea(worker1.getPassengerQueue());
-		area3.setEditable(false);
+		areaPassengers = new JTextArea(worker1.getPassengerQueue());
+		areaPassengers.setEditable(false);
 
-        JScrollPane scroll1 = new JScrollPane(area3);
+        JScrollPane scroll1 = new JScrollPane(areaPassengers);
         scroll1.setPreferredSize(new Dimension(180,240)); 
         scroll1.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 
         nPanel.add(scroll1);
 		
-		area4 = new JTextArea(worker1.getTaxiQueue());
-		area4.setEditable(false);
+		areaTaxis = new JTextArea(worker1.getTaxiQueue());
+		areaTaxis.setEditable(false);
 
-        JScrollPane scroll2 = new JScrollPane(area4);
+        JScrollPane scroll2 = new JScrollPane(areaTaxis);
         scroll2.setPreferredSize(new Dimension(180,240)); 
         scroll2.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 
         nPanel.add(scroll2);
+        
 
 		this.add(nPanel, BorderLayout.SOUTH);
 		
@@ -162,11 +206,17 @@ public class MVCKioskView extends JFrame implements Observer, ActionListener{
 	public void go() {
 		
 		worker1.ready();
-		worker1.ready();
+		worker2.ready();
+		worker3.ready();
+		worker4.ready();
     	workerThread1 = new Thread(worker1);
     	workerThread2 = new Thread(worker2);
+    	workerThread3 = new Thread(worker3);
+    	workerThread4 = new Thread(worker4);
     	workerThread1.start();
     	workerThread2.start();
+    	workerThread3.start();
+    	workerThread4.start();
 		
 	}
 	
@@ -176,8 +226,10 @@ public class MVCKioskView extends JFrame implements Observer, ActionListener{
 		
 		worker1.setKiosk(kiosk);
 		worker2.setKiosk(kiosk);
+		worker3.setKiosk(kiosk);
+		worker4.setKiosk(kiosk);
 		
-		area3.setText(worker1.getPassengerQueue());
+		areaPassengers.setText(worker1.getPassengerQueue());
 	}
 	
 	public void resetTaxi() {
@@ -186,8 +238,10 @@ public class MVCKioskView extends JFrame implements Observer, ActionListener{
 		
 		worker1.setKiosk(kiosk);
 		worker2.setKiosk(kiosk);
+		worker3.setKiosk(kiosk);
+		worker4.setKiosk(kiosk);
 		
-		area4.setText(worker1.getTaxiQueue());
+		areaTaxis.setText(worker1.getTaxiQueue());
 	}	
 
 	//event listeners
@@ -218,6 +272,8 @@ public class MVCKioskView extends JFrame implements Observer, ActionListener{
 		else if (e.getSource() == stop) {
 			worker1.stopEarly();
 			worker2.stopEarly();
+			worker3.stopEarly();
+			worker4.stopEarly();
 		}
 		else if (e.getSource() == resetPass) {
 			resetPass();
@@ -233,20 +289,19 @@ public class MVCKioskView extends JFrame implements Observer, ActionListener{
 		
 		output1 = worker1.getStatus();
 		area1.setText(output1);
-		//JOptionPane.showMessageDialog(this, "worker 1: \n" + output1);
-		//System.out.println("W1: " + output1);
 		
 		output2 = worker2.getStatus();
 		area2.setText(output2);
-		//JOptionPane.showMessageDialog(this, "worker 2: \n" + output2);
-		//System.out.println("2: " + output2);
+
+		output3 = worker3.getStatus();
+		area3.setText(output3);
 		
-		//passLabel.setText("QUEUE (" + worker1.getNumPassengerGroups() + ")");
-		area3.setText(worker1.getPassengerQueue());
-		area4.setText(worker1.getTaxiQueue());
-		//taxiLabel.setText("TAXIS (" + worker1.getNumTaxis() + ")");
+		output4 = worker4.getStatus();
+		area4.setText(output4);
 		
-		//pack();
+		areaPassengers.setText(worker1.getPassengerQueue());
+		areaTaxis.setText(worker1.getTaxiQueue());
+
 		
 	}
 	
